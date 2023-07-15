b1f704bb440b04a4f61a0160cf5782e18159c99c
Parijat-Majumder <115878618+Parijat-Majumder@users.noreply.github.com>
2023-07-15 22:28:06 +0530

    Update Day4FINAL_ROCKPAPER.py

--- Day4FINAL_ROCKPAPER.py
+++ Day4FINAL_ROCKPAPER.py
@@ -3,24 +3,65 @@ ROCK PAPER SCISSOR GAME
 """
 
 
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
 #Write your code below this line 👇
+images = [rock , paper , scissors]
+
 import random
+
 user_input = int(input("What do you want to choose ?0 for Rock 1 for Paper and 2 for Scissor?\n"))
+
+print(images[user_input])
+
 computer_choice = random.randint(0,2) 
-print(computer_choice)
+
+print(f"computer_choice:{computer_choice}")
+print(images[computer_choice])
+
 if user_input >= 3 or user_input<0   :
   print("You typed a wrong number ,You lose!")
+  
 elif user_input == 0 and computer_choice == 1:
   print("You lose!")
+  
 elif user_input == 0 and computer_choice == 2:
   print("You Won!")
+  
 elif user_input == 2 and computer_choice == 0:
   print("You lose!") 
+  
 elif user_input == 1 and computer_choice == 2:
   print("You lose!")
 
 elif user_input > computer_choice :
   print("You win!")
+  
 elif user_input == computer_choice :
   print("Match Draw")
 
